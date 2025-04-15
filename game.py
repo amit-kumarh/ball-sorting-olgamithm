ba2f00396e904e767ba244c57a3b403a4bbc9758
Amit Kumar <amitkh@gmail.com>
2025-04-15 15:19:58 -0400

    start the game

--- game.py
+++ game.py
@@ -1,20 +1,44 @@
+from enum import Enum
+
+class Color(Enum):
+    RED = 0
+    ORANGE = 1
+    YELLOW = 2
+    GREEN = 3
+    BLUE = 4
+    INDIGO = 5
+    VIOLET = 6
+
 class BallSorter:
-    def __init__(self, config, max_in_col) -> None:
-        self.num_cols = len(config)
+    def __init__(self, initial_config, max_in_col) -> None:
+        self.num_cols = len(initial_config)
         self.max_in_col = max_in_col
-        self.config = config
+        self.state = initial_config
 
     def is_valid_move(self, src, dest):
-        pass
+        # check top of src matches top of dest
+        if self.state[src][-1] == self.state[dest][-1] and len(self.state[dest]) < self.max_in_col:
+            return True
+        return False
 
     def move(self, src, dest):
-        pass
+        self.state[dest].append(self.state[src].pop())
 
     def done(self):
-        pass
+        for col in self.state:
+            if len(set(col)) > 1:
+                return False
+        return True
 
     def get_neighbors(self):
-        pass
+        neighbors = []
+        for si, _ in enumerate(self.state):
+            for di, _ in enumerate(self.state):
+                if si == di:
+                    continue
+                if self.is_valid_move(si, di):
+                    neighbors.append((si, di))
+        return neighbors
 
     def heuristic(self):
         pass
