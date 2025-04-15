4605e629d90effabbd521785bedf34534c0348de
Amit Kumar <amitkh@gmail.com>
2025-04-15 15:10:09 -0400

    game skeleton

---
+++ game.py
@@ -0,0 +1,21 @@
+class BallSorter:
+    def __init__(self, config, max_in_col) -> None:
+        self.num_cols = len(config)
+        self.max_in_col = max_in_col
+        self.config = config
+
+    def is_valid_move(self, src, dest):
+        pass
+
+    def move(self, src, dest):
+        pass
+
+    def done(self):
+        pass
+
+    def get_neighbors(self):
+        pass
+
+    def heuristic(self):
+        pass
+        
