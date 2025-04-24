80d6556e34637937def9659855c8d4c020c78b74
Amit Kumar <amitkh@gmail.com>
2025-04-24 19:09:45 -0400

    amend run.py to do path len and states explored

--- run.py
+++ run.py
@@ -9,28 +9,39 @@ def main():
     with open("games.pickle", "rb") as pf:
        games = pickle.load(pf)
 
-    res = []
-    for game in games:
-        print(game)
-        path, score = a_star(game)
-        print(score)
-        res.append(score)
+    scores_dict = {}
+    states_dict = {}
+    for heuristic, name in zip(heuristics, names):
+        scores = []
+        states = []
+        for game in games:
+            print(game)
+            path, num_states = a_star(game)
+            print(len(path), states)
+            states.append(num_states)
+            scores.append(len(path))
+        scores_dict[name] = scores
+        states_dict[name] = states
 
     with open("results.json", "w") as of:
-        json.dump({"Heuristic 1": res}, of)
-
-
+        json.dump([scores_dict, states_dict], of)
 
 def plot():
     import matplotlib.pyplot as plt
 
     with open("results.json", "r") as in_f:
-        res = json.load(in_f)
+        scores_dict, states_dict = json.load(in_f)
 
-    plt.boxplot(res.values(), tick_labels=list(res.keys()))
+    plt.boxplot(states_dict.values(), tick_labels=list(states_dict.keys()))
     plt.xlabel("Heuristic Function")
     plt.ylabel("States Explored")
     plt.title("Ball Sorter Heuristic Performance")
+    plt.show()
+
+    plt.boxplot(scores_dict.values(), tick_labels=list(scores_dict.keys()))
+    plt.xlabel("Heuristic Function")
+    plt.ylabel("Number of moves to solved")
+    plt.title("Ball Sorter Path Length")
     plt.show()
 
 if __name__ == "__main__":
